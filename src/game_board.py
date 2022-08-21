f6e16fdd12d2192e6dd2c1c077f9ee9ceaa1920a
blazin-byte <80867520+blazin-byte@users.noreply.github.com>
2022-08-21 14:42:16 -0400

    Gameboard

--- src/game_board.py
+++ src/game_board.py
@@ -2,7 +2,30 @@ import pygame as pg
 
 
 class GameBoard():
-    def __init__(self) -> None:
+    """ Positions will be a list of lists representing the coordinates of the the ships given by each bot"""
+
+    def __init__(self, positions) -> None:
+        self.matrix_of_tiles = []
+        self.length = 8
+        self.width = 8
+        self.matrix = [
+            [Tile(i, j) for i in len(self.width)] for j in len(self.length)
+        ]
+
+    def init_ships(positions):
+        ships = []
+        for coords in positions:
+            ships.append(Ship(len(coords)))
+
+
+# are all of your ships sunk
+# Update ships positions
+# pregame reset (reset ship coordinates, reset tiles states)
+
+# Do I get the ship positions from the bot? Can I assume I'll just get my ship coordinates as a list from the bot?
+
+
+    def all_sunk():
         pass
 
     def reset_before_game():
@@ -23,6 +46,7 @@ class Tile():
     def __init__(self, row, column) -> None:
         self.row = row
         self.column = column
+        self.hit = False
         self.num_hits = 0
 
     def reset_before_game():
