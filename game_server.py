eb524f39a6dd4daa01798ab5db4fd80ce09191a9
blazin-byte <80867520+blazin-byte@users.noreply.github.com>
2022-08-21 13:18:16 -0400

    Display Window

--- game_server.py
+++ game_server.py
@@ -1,9 +1,35 @@
 import pygame
 
+
 def main():
-    pass
+
+    background_colour = (234, 212, 252)
+    # Define the dimensions of
+    # screen object(width,height)
+    screen = pygame.display.set_mode((1200, 600))
+
+    # Set the caption of the screen
+    pygame.display.set_caption('Geeksforgeeks')
+
+    # Fill the background colour to the screen
+    screen.fill(background_colour)
+
+    # Update the display using flip
+    pygame.display.flip()
+
+    # Variable to keep our game loop running
+    running = True
+
+    # game loop
+    while running:
+
+        # for loop through the event queue
+        for event in pygame.event.get():
+
+            # Check for QUIT event
+            if event.type == pygame.QUIT:
+                running = False
 
 
 if __name__ == "__main__":
     main()
-    
